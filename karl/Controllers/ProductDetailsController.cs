a276f82483e11dae3a0d640898692284699c26b9
sandifouad <44641093+sandifouad@users.noreply.github.com>
2018-11-27 16:39:19 +0200

    Initial commit

---
+++ karl/Controllers/ProductDetailsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace karl.Controllers
+{
+    public class ProductDetailsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult productdetails()
+        {
+            return View();
+        }
+        public IActionResult cart()
+        {
+            return View();
+        }
+    }
+}
